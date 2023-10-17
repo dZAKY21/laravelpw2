3a6a9a1298aa703fd4fdc254744fe5b002d00609
dZAKY21 <mohammaddzaky_2226250083@mhs.mdp.ac.id>
2023-10-17 15:18:13 +0700

    commit laravel

---
+++ resources/views/fakultas.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+@section('title', 'Fakultas')
+@section('content')
+    <center>
+        <h1>Halaman Fakultas</h1>
+        <p>Ini adalah halaman fakultas </p>
+    </center>
+@endsection
---
+++ resources/views/prodi.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+@section('title', 'Program Studi')
+@section('content')
+    <center>
+        <h1>Halaman Program Studi</h1>
+        <p>Ini adalah halaman Program Studi </p>
+    </center>
+@endsection
