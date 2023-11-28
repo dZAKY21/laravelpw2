c862983a0b2a218af4663c30be8dfa90477a100c
dZAKY21 <130964137+dZAKY21@users.noreply.github.com>
2023-11-28 15:14:22 +0700

    Add home_controller, home_blade

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -27,6 +27,15 @@
                 <li class="nav-item sidebar-category">
                     <p>Navigation</p>
                     <span></span>
+
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('home') }}">
+                        <i class="mdi mdi-home-modern menu-icon"></i>
+                        <span class="menu-title">Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-item sidebar-category">
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('fakultas') }}">
