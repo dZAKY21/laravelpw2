92cc95bf57c186ec037274aa4132616fe0dc0ae5
dZAKY21 <130964137+dZAKY21@users.noreply.github.com>
2023-12-19 15:13:20 +0700

    add api/authController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\FakultasController;
 use App\Http\Controllers\API\MahasiswaController;
 use App\Http\Controllers\API\ProdiController;
@@ -20,6 +21,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get(
+    'fakultas',
+    [FakultasController::class, 'index']
+);
+Route::middleware(['auth:sanctum', 'ability:read-fakultas'])->get('fakultas', [FakultasController::class, 'index']);
+Route::middleware(['auth:sanctum', 'ability:create-fakultas'])->post('fakultas', [FakultasController::class, 'store']);
+Route::middleware(['auth:sanctum', 'ability:update-fakultas'])->patch('fakultas/{id}', [FakultasController::class, 'update']);
+Route::middleware(['auth:sanctum', 'ability:delete-fakultas'])->delete('fakultas/{id}', [FakultasController::class, 'destroy']);
 
 Route::get('fakultas', [FakultasController::class, 'index']);
 Route::get('prodi', [ProdiController::class, 'index']);
